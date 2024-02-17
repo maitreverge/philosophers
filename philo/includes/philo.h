7c86e8860571632a2b26a063319618701ce63cb4
maitreverge <verge.f@gmail.com>
2024-02-17 21:37:50 +0100

    🚀 INIT PHILO (working Makefile)

---
+++ philo/includes/philo.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/17 21:18:18 by flverge           #+#    #+#             */
+/*   Updated: 2024/02/17 21:31:17 by flverge          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>   // memset
+# include <stdio.h>    // printf
+# include <stdlib.h>   // malloc, free
+# include <unistd.h>   // write, usleep
+# include <sys/time.h> // gettimeofday
+# include <pthread.h>
+//         ||
+//         V 
+// pthread_create, pthread_detach, pthread_join, pthread_mutex_init
+// pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock
+
+
+
+
+#endif
