6582a0831f4bb9ae1c501545a1dfec1e27f3c4cf
flverge_42 <verge.f@gmail.com>
2024-02-21 11:27:39 +0100

    🚧 WORKING ON : understanding threads

---
+++ test/testing.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void*	routine()
+{
+	printf("Start of the thread \n");
+	sleep(3);
+	printf("End of the thread \n");
+	// sleep(3);
+}
+
+int main(int ac, char **av)
+{
+	// ! STEP 1 : creation
+	pthread_t t1, t2;
+	/*
+	to hold a thread during an program execution, we first need to
+	create a variable type which is gonna be of type pthread.
+	It kind of storing the thread
+	
+	*/
+
+	// ! STEP 2 : init the thread
+	/*
+	Then, we need to create the thread itself with a few arguments :
+	&t1, NULL, &routine, NULL
+	Adress of the variable |the attributes, regarding customizations | the actual function | the arguments to the function)
+
+	*/
+
+	// ! STEP 2.1 : error checking
+	/*
+	for whatever reason, the thread creation could fail, the execution itself could fail
+	that's why we need to check if the thread execution went okay
+	*/
+
+	/*
+	SO instead of just calling the threads rwa like this :
+	pthread_create(&t1, NULL, &routine, NULL);
+	pthread_create(&t2, NULL, &routine, NULL);
+
+	We could call them in a if statement like :
+	
+
+	*/
+
+
+
+	// ! STEP 3 : WAIT
+	/*
+	Because we don't want the main function to finish during the thread running
+	*/
+
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+
+	return 0;
+}
