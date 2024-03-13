8455f43abac68b24306d199a060612cd627d1d01
maitreverge <verge.f@gmail.com>
2024-03-13 14:32:43 +0100

    save clock

---
+++ test/clock.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <limits.h>
+#include <sys/time.h>
+#include <time.h>
+
+void	function_1(void)
+{
+	int a = INT_MAX;
+
+	while (a != 0)
+		a--;
+}
+
+int main(void)
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	printf("Start = %li\n", tv.tv_sec);
+
+	function_1();
+
+	gettimeofday(&tv, NULL);
+	printf("End = %li\n", tv.tv_sec);
+}
