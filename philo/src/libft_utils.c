2718fa974519ee5eb6a909fe6a9e6715f7896de6
“florianverge” <verge.f@gmail.com>
2024-03-14 17:49:19 +0100

    simplified nubers checking functions

--- philo/src/libft_utils.c
+++ philo/src/libft_utils.c
@@ -6,47 +6,39 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 12:58:37 by flverge           #+#    #+#             */
-/*   Updated: 2024/03/13 11:36:58 by flverge          ###   ########.fr       */
+/*   Updated: 2024/03/14 17:36:03 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-static int	ft_isspace(int c)
+static bool	ft_isspace(int c)
 {
 	if ((c >= 9 && c <= 13) || c == 32)
-		return (1);
-	return (0);
+		return true;
+	return false;
 }
 
-int	ft_atoi(const char *str)
+size_t	ft_atoi(const char *str)
 {
 	int	i;
-	int	polarity;
-	int	result;
+	size_t	result;
 
 	i = 0;
-	polarity = 1;
 	result = 0;
-	while (ft_isspace(str[i]) == 1)
+	while (ft_isspace(str[i]))
 		i++;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			polarity *= -1;
-		i++;
-	}
-	while (ft_isdigit(str[i]) == 1)
+	while (ft_isdigit(str[i]))
 	{
 		result = (10 * result) + (str[i] - '0');
 		i++;
 	}
-	return (polarity * result);
+	return (result);
 }
 
-int	ft_isdigit(int c)
+bool	ft_isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
+		return true;
+	return false;
 }
