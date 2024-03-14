913e4aff50960e377073ef7d13a7553a016fd05c
“florianverge” <verge.f@gmail.com>
2024-03-14 17:50:03 +0100

    🚧 WORKING ON : working pre_diner parsing

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 21:18:38 by flverge           #+#    #+#             */
-/*   Updated: 2024/03/13 18:08:01 by flverge          ###   ########.fr       */
+/*   Updated: 2024/03/14 17:43:40 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,19 @@ t_pars *init_struct(int ac, char **av)
 	if (!new_node)
 		return NULL;
 	new_node->nb_philos = ft_atoi(av[1]);
-	new_node->time2die = ft_atoi(av[2]);
-	new_node->time2eat = ft_atoi(av[3]);
-	new_node->time2sleep = ft_atoi(av[4]);
+	new_node->time2die = ft_atoi(av[2]) * 1000; // conversion to microseconds
+	new_node->time2eat = ft_atoi(av[3]) * 1000;
+	new_node->time2sleep = ft_atoi(av[4]) * 1000;
 	if (ac == 6 && av[5])
-		new_node->nb_meals = ft_atoi(av[5]);
+		new_node->max_meals = ft_atoi(av[5]);
 
 	return (new_node);
+}
+
+void	custom_exit(char *s)
+{
+	printf("%s\n");
+	exit(EXIT_FAILURE);
 }
 
 int main(int ac, char **av)
@@ -36,8 +42,15 @@ int main(int ac, char **av)
 	data = NULL;
 	if (arg_checker(ac, av) == true)
 	{
+		// ! Need to init philo and fork structs
 		data = init_struct(ac, av);
-		print_struct(&data);
+		if (data_wrong_time(&data))
+			custom_exit("Time should be above 60ms");
+		// print_struct(&data);
+		// ! start the dinner
+		// code
+		
+		// ! clean all the stuff
 		free(data);
 	}
 	return (0);
